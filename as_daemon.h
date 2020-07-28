c091789df98b353688f3b86e94649ffff6f7a6db
timehunter <shrong.he@gmail.com>
2020-07-28 14:14:13 +0800

    daemon

--- as_daemon.h
+++ as_daemon.h
@@ -11,18 +11,16 @@ extern "C" {
 
 typedef enum
 {
-    enForeGround          = 0, //前台运行
-    enBackGround          = 1 //后台运行
+    enForeGround          = 0, 
+    enBackGround          = 1 
 }RUNNING_MOD;
 
-
-/*iRunningMod : 1  守护进程模式，0 前台模式*/
 void as_run_service(void(*pWorkFunc)(),
                     int32_t iRunningMod,
                     void (*pExitFunc)(),
                     const char* service_conf_path,
                     int32_t service_id );
-//退出
+//锟剿筹拷
 void send_sigquit_to_deamon();
 
 #ifdef __cplusplus
