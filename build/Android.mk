d7d467db6486b24e929c2214d55871a602ff332f
timehunter <shrong.he@gmail.com>
2021-02-04 17:33:42 +0800

    ndk

--- build/Android.mk
+++ build/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CFLAGS += -DOS_LINUX -DOS_ANDROID  -pipe -fPIC -g -Wall -O0 -DSOCKLEN_T=socklen_t -D_LARGEFILE_SOURCE=1 -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE
-LOCAL_LDLIBS += -lm -lpthread -ldl
+LOCAL_LDLIBS += -lm -pthread -ldl
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../inc
