9e21a6abb31ba90f448223ce20d27171ad00c886
timehunter <shrong.he@gmail.com>
2021-02-04 16:09:39 +0800

    ndk

--- build/Android.mk
+++ build/Android.mk
@@ -3,6 +3,8 @@ LOCAL_PATH := $(call my-dir)
 AS_INCLUDE := ${LOCAL_PATH}/../inc/
 AS_SOURCE  := ${LOCAL_PATH}/../src/
 
+NDK_TOOLCHAIN_VERSION := clang
+
 include $(CLEAR_VARS)
 LOCAL_MODULE     := common
 LOCAL_C_INCLUDES := $(AS_INCLUDE)
@@ -10,3 +12,4 @@ LOCAL_SRC_FILES  := ${AS_SOURCE}*.c  ${AS_SOURCE}*.cpp
 
 include $(BUILD_SHARED_LIBRARY)
 LOCAL_LDLIBS     +=  -ldl -lm -lpthread
+LOCAL_CFLAGS     := -mllvm -fla
