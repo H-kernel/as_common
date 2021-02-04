9cd0038e48b0ba0e008c20af9f4ea173603be049
timehunter <shrong.he@gmail.com>
2021-02-04 16:05:24 +0800

    ndk

--- build/Android.mk
+++ build/Android.mk
@@ -4,7 +4,7 @@ AS_INCLUDE := ${LOCAL_PATH}/../inc/
 AS_SOURCE  := ${LOCAL_PATH}/../src/
 
 include $(CLEAR_VARS)
-LOCAL_MODULE     := hello-jni
+LOCAL_MODULE     := common
 LOCAL_C_INCLUDES := $(AS_INCLUDE)
 LOCAL_SRC_FILES  := ${AS_SOURCE}*.c  ${AS_SOURCE}*.cpp
 
